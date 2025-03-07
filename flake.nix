e3a5bcc3748d93e49e7e227cef7abd5153859e3a
Alex Vegas <alexrbvegas@gmail.com>
2025-03-07 00:57:37 -0500

    tabs

--- flake.nix
+++ flake.nix
@@ -39,6 +39,22 @@
             inherit self inputs username;
           };
         };
+        laptop = nixpkgs.lib.nixosSystem {
+          inherit system;
+          modules = [ ./hosts/laptop ];
+          specialArgs = {
+            host = "laptop";
+            inherit self inputs username;
+          };
+        };
+        desktop = nixpkgs.lib.nixosSystem {
+          inherit system;
+          modules = [ ./hosts/desktop ];
+          specialArgs = {
+            host = "desktop";
+            inherit self inputs username;
+          };
+        };
         
       };
   };
