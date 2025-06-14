cf2ed33aaf781d1d08642d87a4ca0d0677cedb8e
Alex Vegas <alexrbvegas@gmail.com>
2025-06-13 22:19:30 -0400

    bruh

--- modules/home/editor/codium.nix
+++ modules/home/editor/codium.nix
@@ -1,5 +1,12 @@
-{ pkgs, ... }: {
-  programs.codium = {
+{ pkgs, ... }:
+{
+  programs.vscode = {
     enable = true;
+    package = pkgs.vscodium;
+    profiles.default.extensions = with pkgs.vscode-extensions; [
+      dracula-theme.theme-dracula
+      vscodevim.vim
+      yzhang.markdown-all-in-one
+    ];
   };
 }
